79251cdee858f0d965b08ad51b4b93d80df95318
minhvu <vd.minh@outlook.com>
2026-03-27 11:29:48 +0700

    refactor role service

--- src/framework/src/Identity.EntityFrameworkCore/Extensions/CollectionSyncExtensions.cs
+++ src/framework/src/Identity.EntityFrameworkCore/Extensions/CollectionSyncExtensions.cs
@@ -5,6 +5,27 @@ public class CollectionSyncExtensions
     public static async Task SyncCollectionAsync<T>(
         IEnumerable<T> current,
         IEnumerable<T> target,
+        Func<T, Task> removeAsync,
+        Func<T, Task> addAsync,
+        IEqualityComparer<T>? comparer = null)
+    {
+        comparer ??= EqualityComparer<T>.Default;
+
+        var currentSet = current.ToHashSet(comparer);
+        var targetSet = target.ToHashSet(comparer);
+
+        var toRemove = currentSet.Except(targetSet, comparer);
+        foreach (var item in toRemove)
+            await removeAsync(item).ConfigureAwait(false);
+
+        var toAdd = targetSet.Except(currentSet, comparer);
+        foreach (var item in toAdd)
+            await addAsync(item).ConfigureAwait(false);
+    }
+
+    public static async Task SyncCollectionAsync<T>(
+        IEnumerable<T> current,
+        IEnumerable<T> target,
         Func<IEnumerable<T>, Task> removeAsync,
         Func<IEnumerable<T>, Task> addAsync,
         IEqualityComparer<T>? comparer = null)
