3a0f819e7e467701710888f5c272ffc8e32ba9b9
minhvu <vd.minh@outlook.com>
2026-03-28 21:25:41 +0700

    fix core

---
+++ src/framework/src/Extensions/ClaimExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+
+namespace Light.Extensions
+{
+    public static class ClaimExtensions
+    {
+        public static List<Claim> Add(this List<Claim> claims, string key, string? value)
+        {
+            if (!string.IsNullOrEmpty(value))
+            {
+                claims.Add(new Claim(key, value));
+            }
+
+            return claims;
+        }
+
+        public static string? FindFirstValue(this ClaimsPrincipal principal, string claimType) =>
+            principal is null
+                ? throw new ArgumentNullException(nameof(principal))
+                : principal.FindFirst(claimType)?.Value;
+    }
+}
--- src/framework/src/SharedKernel/Domain/Entities/DomainEvent.cs
+++ src/framework/src/SharedKernel/Domain/Entities/DomainEvent.cs
@@ -1,10 +1,12 @@
+using Light.Mediator;
+
 namespace Light.Domain.Entities;
 
 /// <summary>
 ///     A base type for domain events. Depends on MediatR INotification.
 ///     Includes DateOccurred which is set on creation.
 /// </summary>
-public abstract class DomainEvent
+public abstract class DomainEvent : INotification
 {
     public virtual DateTimeOffset TriggeredOn { get; protected set; } = DateTimeOffset.UtcNow;
 }
