d114f1c0ba66c86e9adec1ce7fe4a6256595516d
minhvu <vd.minh@outlook.com>
2026-05-17 16:27:38 +0700

    vendor: update-microsoft-graph

--- src/services/samples/WebApi/Controllers/GraphController.cs
+++ src/services/samples/WebApi/Controllers/GraphController.cs
@@ -5,8 +5,27 @@ namespace WebApi.Controllers
 {
     [Route("[controller]")]
     [ApiController]
-    public class GraphController(IGraphTeams graphTeams) : ControllerBase
+    public class GraphController(
+        IGraphMailService graphMailService,
+        IGraphTeams graphTeams) : ControllerBase
     {
+        [HttpGet("send_email")]
+        public async Task<IActionResult> SendMail()
+        {
+            await graphMailService.SendAsync(
+                new Light.Mail.MailFrom("test@yopmail.com"),
+                new Light.Mail.MailMessage
+                {
+                    Recipients = ["test@yopmail.com"],
+                    Subject = "Test",
+                    Content = "Test Body"
+                }
+            );
+
+            return Ok();
+        }
+
+
         [HttpGet]
         public async Task<IActionResult> Get(string user)
         {
