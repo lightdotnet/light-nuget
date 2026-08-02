61a01416c7e392ea7974f48d586ed1c11bc8ad28
minhvu <vd.minh@outlook.com>
2026-08-02 16:41:17 +0700

    feat(auth): add permission definition

---
+++ src/framework/src/Authorization/AspNetCore/Authorization/IPermissionManager.cs
@@ -0,0 +1,6 @@
+﻿namespace Light.AspNetCore.Authorization;
+
+public interface IPermissionManager
+{
+    IReadOnlyCollection<PermissionDefinition> GetPermissions();
+}
---
+++ src/framework/src/Authorization/AspNetCore/Authorization/PermissionDefinition.cs
@@ -0,0 +1,13 @@
+﻿namespace Light.AspNetCore.Authorization;
+
+public sealed class PermissionDefinition(
+    string name,
+    string displayName,
+    string? parent = null)
+{
+    public string Name { get; } = name;
+
+    public string DisplayName { get; } = displayName;
+
+    public string? Parent { get; } = parent;
+}
