884afea740586653e067203828b618d032776aa8
minhvu <vd.minh@outlook.com>
2026-08-11 10:14:39 +0700

    refactor: SMTP options

--- src/services/src/SmtpMail/SmtpMailOptions.cs
+++ src/services/src/SmtpMail/SmtpMailOptions.cs
@@ -4,21 +4,17 @@ namespace Light.SmtpMail
     {
         public string Host { get; set; } = null!;
 
-        public int Port { get; set; } = 25;
+        public virtual int Port { get; set; } = 25;
 
         public bool UseSsl { get; set; }
     }
 
-    public class SmtpMailKitOptions
+    public class SmtpMailKitOptions : SmtpMailOptions
     {
-        public string Host { get; set; } = null!;
-
-        public int Port { get; set; } = 587;
+        public override int Port { get; set; } = 587;
 
         public string UserName { get; set; } = null!;
 
         public string Password { get; set; } = null!;
-
-        public bool UseSsl { get; set; }
     }
 }
